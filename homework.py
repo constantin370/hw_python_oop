7330f9acd6b4e80effdccf9614194feed3b113e8
Константин Мурадян <constantin37@gmail.com>
2023-09-05 21:52:58 +0300

    final

--- homework.py
+++ homework.py
@@ -1,3 +1,17 @@
+# Финальный проект спринта: модуль фитнес-трекера
+from time import sleep
+
+
+def check_data(num: str) -> float:
+    try:
+        num = float(num)
+        return num
+    except ValueError:
+        print("\nERROR", ValueError)
+        sleep(3)
+        exit()
+
+
 class InfoMessage:
     """Информационное сообщение о тренировке."""
     def __init__(self, training_type,
@@ -28,9 +42,9 @@ class Training:
 
     def __init__(self, action: int, duration: float, weight: float) -> None:
 
-        self.action = action
-        self.duration = duration
-        self.weight = weight
+        self.action = check_data(action)
+        self.duration = check_data(duration)
+        self.weight = check_data(weight)
         self.training_time_in_min = self.duration * self.MIN_IN_H
 
     def get_distance(self) -> float:
@@ -90,7 +104,7 @@ class SportsWalking(Training):
                  duration: float,
                  weight: float,
                  height) -> None:
-        self.height = height
+        self.height = check_data(height)
         super().__init__(action, duration, weight)
 
     def get_spent_calories(self) -> float:
@@ -113,8 +127,8 @@ class Swimming(Training):
     def __init__(self, action: int, duration: float, weight: float,
                  length_pool: float, count_pool: float) -> None:
         super().__init__(action, duration, weight)
-        self.length_pool = length_pool
-        self.count_pool = count_pool
+        self.length_pool = check_data(length_pool)
+        self.count_pool = check_data(count_pool)
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
@@ -150,17 +164,23 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training) -> str:
     """Главная функция."""
-    info = training.show_training_info()
-    return print(info.get_message())
+    try:
+        info = training.show_training_info()
+        return print(info.get_message())
+    except AttributeError:
+        print(f'ERROR {AttributeError}')
 
 
 if __name__ == '__main__':
-    packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
-    ]
-
-    for workout_type, data in packages:
-        training = read_package(workout_type, data)
-        main(training)
+    try:
+        packages = [
+            ('SWM', [720, 1, 80, 25, 40]),
+            ('RUN', [15000, 1, 75]),
+            ('WLK', [9000, 1, 75, 180]),
+        ]
+        for workout_type, data in packages:
+            training = read_package(workout_type, data)
+            main(training)
+    except NameError:
+        sleep(3)
+        print(f'ERROR {NameError}')
